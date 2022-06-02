59dc94817ada83eb0bb7ff9f726713fab6d09b65
jensenrex <jensenrex@gmail.com>
2022-06-02 11:26:06 +1000

    finish project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import Review from './Review';
-function App() {
-  return <h2>reviews project setup</h2>;
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {  
+  return (
+  <main>
+    <div className="card">
+      <h5 className="card-title">Our reviews</h5>
+      <Review />
+      </div>
+  </main>
+  )
 }
 
 export default App;
--- src/Review.js
+++ src/Review.js
@@ -3,7 +3,57 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const {name, job, image, text} = people[index]; 
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0; 
+    } 
+    if (number < 0) {
+      return (people.length - 1); 
+    }
+    return number; 
+  }
+  function nextPerson() {
+    setIndex((index)=> {
+      let newIndex = index+1; 
+      return checkNumber(newIndex); 
+    });
+  }; 
+
+  const prevPerson = () => {
+    setIndex(checkNumber(index-1)); 
+  }
+
+  const randomPerson = () => {
+    let ranNumber = Math.floor((Math.random() * people.length)); 
+    if (ranNumber == index) {
+      ranNumber = index + 1; 
+    }
+    setIndex(checkNumber(ranNumber)); 
+  }
+
+  return (
+    <div className="card-body">
+      <img src={image} className="img-thumbnail rounded mx-auto d-block" alt={name} />
+      <span className="quote-icon"><FaQuoteRight /></span>      
+      <div className="text-center text-capitalize">
+
+      <p className="h4  text-primary">{name}</p>
+      <figcaption class="blockquote-footer">
+        <cite>{job}</cite>
+      </figcaption>
+      </div>
+      <p className="card-text">{text}</p>
+      <div className="btn-group btn-group-lg" role="group">
+        <button onClick={prevPerson}><FaChevronLeft/></button>
+        <button className="btn-outline-primary" onClick={randomPerson}>Surprise Me</button>
+        <button onClick={nextPerson}><FaChevronRight/></button>
+      </div>
+    </div>
+  )
+
 };
 
 export default Review;
